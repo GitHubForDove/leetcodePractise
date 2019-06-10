c3e38a9da3ff038804cd40ce2161f6193325d8d3
李迎 <liying84@maoyan.com>
2019-06-10 13:47:37 +0800

    leetcode编程题集合 git项目 第一次上传

---
+++ src/main/java/com/example/leetcode/demo/partition/DiffWaysToCompute.java
@@ -0,0 +1,19 @@
+package com.example.leetcode.demo.partition;
+
+/**
+ * Copyright (c) 2019 maoyan.com
+ * All rights reserved.
+ *
+ * @author liying
+ * @created 2019-06-10
+ * @descript 给表达式加括号  leetcode 241. Different Ways to Add Parentheses (Medium)
+ */
+public class DiffWaysToCompute {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+
+
+}
