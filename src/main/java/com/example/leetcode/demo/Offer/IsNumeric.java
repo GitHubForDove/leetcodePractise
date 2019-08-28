02d7f218154e8209805fcf454845a8b8af077a78
liying84 <liying84@maoyan.com>
2019-08-28 23:25:27 +0800

    add annocation

--- src/main/java/com/example/leetcode/demo/Offer/IsNumeric.java
+++ src/main/java/com/example/leetcode/demo/Offer/IsNumeric.java
@@ -16,8 +16,19 @@ public class IsNumeric {
      * 例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。
      * 但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
      */
-    static char[] number = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
+    /**
+     *
+     * 1、字符为 e 或者 E 时： 1）e后面一定要接数字，没有接数字为不正确的数字
+     *                      2）不能存在有那个e的情况
+     *
+     * 2、当字符为 + 或者 - 时：1）如果出现的位置不是首位，那么只能出现在e的后面 如果出现在其他位置 直接返回false
+     *                       2）第二次只能在 e 后面
+     *
+     * 3、当字符为 . 时： 1）e后面不能为小数
+     *                  2）小数点后面  只能为数字
+     *
+     */
 
     public static boolean isNumeric(char[] str) {
 
@@ -29,10 +40,10 @@ public class IsNumeric {
             if (str[i] == 'e' || str[i] == 'E') {
 
                 if (i == str.length-1) {
-                    return false; // e后面一定要接数字
+                    return false; // e后面一定要接数字,如果e后面没有数字，就说明不是正确的数字
                 }
                 if (hasE) {
-                    return false;  // 不能同时存在两个e
+                    return false;  // 不能同时存在两个e,
                 }
                 hasE = true;
 
